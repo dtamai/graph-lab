3aadd22cac480a94828fcc3409eb45f249a04baf
Daniel Tamai <daniel.tamai@gmail.com>
2014-03-22 11:52:45 -0300

    Rename files

---
+++ models.rb
@@ -0,0 +1,16 @@
+require_relative 'models/node'
+require_relative 'models/edge'
+require_relative 'models/graph'
+require_relative 'models/barabasi_albert'
+
+g = Graph.new(1, "BA")
+
+1.upto(20) do |n|
+  g.nodes << Node.new(n)
+end
+
+BarabasiAlbert.new(g).apply
+
+g.write_csv
+
+
