cc63530922e5eeaa07d9508e1eec29cb78137a29
Daniel Tamai <daniel.tamai@gmail.com>
2014-03-23 15:54:18 -0300

    Add type (directed/undirected)

--- models/edge.rb
+++ models/edge.rb
@@ -1,19 +1,20 @@
 class Edge
-  attr_reader :id, :label
+  attr_reader :id, :label, :type
   attr_accessor :source, :target
 
-  def initialize(id, label="", source, target)
+  def initialize(id, label="",type="undirected", source, target)
     @id = id
     @label = label
     @source = source
     @target = target
+    @type = type
   end
 
   def self.csv_header
-    "id,source,target"
+    "id,source,target,type"
   end
 
   def to_csv
-    "#{id},#{source},#{target}"
+    "#{id},#{source},#{target},#{type}"
   end
 end
