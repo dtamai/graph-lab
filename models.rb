e7ce6b37b5841c5af2125fd9400f84c56affab87
Daniel Tamai <daniel.tamai@gmail.com>
2014-04-06 01:35:37 -0300

    Add basic tests

--- models.rb
+++ models.rb
@@ -4,17 +4,3 @@ require_relative 'models/graph'
 require_relative 'models/model'
 require_relative 'models/barabasi_albert'
 require_relative 'models/watts_strogatz'
-
-g = Graph.new(1, "BA")
-
-n = 10
-k = 4
-ϐ = 0.2
-
-1.upto(n) do |n|
-  g.nodes << Node.new(n)
-end
-
-WattsStrogatz.apply(g, k, ϐ)
-
-g.write_csv
---
+++ test/Watts_Strogatz_test.rb
@@ -0,0 +1,23 @@
+require_relative 'test_helper'
+
+describe WattsStrogatz do
+
+  before do
+    @nodes = Array.new(10) { |i| Node.new(i) }
+    @graph = Graph.new("test", "WS")
+    @graph.nodes = @nodes
+  end
+
+  describe "::apply" do
+    it "preserves all nodes" do
+      WattsStrogatz.apply(@graph, 4, 0.2)
+      assert_equal @graph.nodes, @nodes
+    end
+
+    it "creates edges" do
+      WattsStrogatz.apply(@graph, 4, 0.2)
+      assert @graph.edges.size > 0
+    end
+  end
+end
+
